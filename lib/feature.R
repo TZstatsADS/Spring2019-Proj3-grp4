4830592878fe6563c2372b84554ec1bf6006ae5d
xl2785 <xl2785@users.noreply.github.com>
2019-03-23 11:00:01 -0700

    Update feature.R

--- lib/feature.R
+++ lib/feature.R
@@ -26,16 +26,16 @@ feature <- function(LR_dir, HR_dir, n_points=1000){
     imgLR <- readImage(paste0(LR_dir,  "img_", sprintf("%04d", i), ".jpg"))
     imgHR <- readImage(paste0(HR_dir,  "img_", sprintf("%04d", i), ".jpg"))
     ### step 1. sample n_points from imgLR
-    
+    x_sam <- sample(1:nrow(imgLR), n_points, replace = T)
+    y_sam <- sample(1:ncol(imgLR), n_points, replace = T)
     ### step 2. for each sampled point in imgLR,
-    
-        ### step 2.1. save (the neighbor 8 pixels - central pixel) in featMat
-        ###           tips: padding zeros for boundary points
-    
-        ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
-    
-    ### step 3. repeat above for three channels
-      
+    temp = array(0, c(dim(imgLR))+c(2,2,0))
+    temp[2:(nrow(imgLR)+1),2:(ncol(imgLR)+1),]= imgLR
+    for (j in 1:n_points) {
+      featMat[(i-1)*n_points+j,,] = matrix(temp[x_sam[j]:(x_sam[j]+2),y_sam[j]:(y_sam[j]+2),],9,3)[-5,]
+      labMat[j,,] = matrix(imgHR[(2*x_sam[j]-1):(2*x_sam[j]),(2*y_sam[j]-1):(2*y_sam[j]),],4,3)
+      }
   }
   return(list(feature = featMat, label = labMat))
 }
+
